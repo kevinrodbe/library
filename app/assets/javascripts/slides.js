57a66dc947f6925bd3f7a1f00b05c5dcd4310878
Eric Allam <rubymaverick@gmail.com>
2012-01-31 11:23:45 -0500

    Adding a 'done' checkbox with just jquery

--- app/assets/javascripts/slides.js
+++ app/assets/javascripts/slides.js
@@ -1,7 +1,15 @@
 $(function(){
   
   $.getJSON('/todo', function(data) {
-    $('#app').append('<h3>' + data.description + '</h3>')
+    $('#app').append('<h3><input type=checkbox class=completed /> ' + data.description + '</h3>')
+
+    $('#app .completed').on('change', function(e){
+      if($(this).is(':checked')){
+        $('#app h3').addClass('done');
+      }else{
+        $('#app h3').removeClass('done');
+      }
+    })
   });
 
 });
