2c0c91d7d89ed7bd1389c259664bb10dc9a6b1f3
Eric Allam <rubymaverick@gmail.com>
2012-09-24 13:33:15 -0400

    Polishing level 1 book

--- Examples/AlphaTouch/AlphaTouch/ViewController.m
+++ Examples/AlphaTouch/AlphaTouch/ViewController.m
@@ -20,10 +20,44 @@
     
     self.view.backgroundColor = [UIColor darkGrayColor];
     UIButton *firstButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    firstButton.frame = CGRectMake(100, 100, 100, 44);
+    [firstButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     [firstButton setTitle:@"Click me!" forState:UIControlStateNormal];
     [firstButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:firstButton];
+    
+    NSArray *cns = [NSLayoutConstraint constraintsWithVisualFormat:@"|-[firstButton]-|"
+                                                           options:NSLayoutAttributeCenterX
+                                                           metrics:nil
+                                                             views:NSDictionaryOfVariableBindings(firstButton)];
+    
+    [self.view addConstraints:cns];
+    
+    cns = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[firstButton]-10-|"
+                                                  options:NSLayoutAttributeBottom metrics:nil
+                                                    views:NSDictionaryOfVariableBindings(firstButton)];
+    
+    [self.view addConstraints:cns];
+    
+//    NSLayoutConstraint *cn =
+//        [NSLayoutConstraint constraintWithItem:firstButton
+//                                     attribute:NSLayoutAttributeCenterX
+//                                     relatedBy:NSLayoutRelationEqual
+//                                        toItem:self.view
+//                                     attribute:NSLayoutAttributeCenterX
+//                                    multiplier:1.0
+//                                      constant:0.0];
+//    [self.view addConstraint:cn];
+//    
+//    cn =
+//        [NSLayoutConstraint constraintWithItem:firstButton
+//                                     attribute:NSLayoutAttributeBottom
+//                                     relatedBy:NSLayoutRelationEqual
+//                                        toItem:firstButton.superview
+//                                     attribute:NSLayoutAttributeBottom
+//                                    multiplier:1.0
+//                                      constant:-10.0];
+//    
+//    [self.view addConstraint:cn];
 }
 
 #define ARC4RANDOM_MAX      0x100000000
