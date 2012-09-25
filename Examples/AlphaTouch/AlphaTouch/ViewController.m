294d1419ef0f43d8e6aedc23994961a9d92030c6
Eric Allam <rubymaverick@gmail.com>
2012-09-25 10:57:25 -0400

    WIP Level 2

--- Examples/AlphaTouch/AlphaTouch/ViewController.m
+++ Examples/AlphaTouch/AlphaTouch/ViewController.m
@@ -18,51 +18,20 @@
 {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor darkGrayColor];
     UIButton *firstButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [firstButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    firstButton.frame = CGRectMake(100, 100, 100, 44);
     [firstButton setTitle:@"Click me!" forState:UIControlStateNormal];
     [firstButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:firstButton];
     
-    NSArray *cns = [NSLayoutConstraint constraintsWithVisualFormat:@"|-[firstButton]-|"
-                                                           options:NSLayoutAttributeCenterX
-                                                           metrics:nil
-                                                             views:NSDictionaryOfVariableBindings(firstButton)];
-    
-    [self.view addConstraints:cns];
-    
-    cns = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[firstButton]-10-|"
-                                                  options:NSLayoutAttributeBottom metrics:nil
-                                                    views:NSDictionaryOfVariableBindings(firstButton)];
-    
-    [self.view addConstraints:cns];
-    
-//    NSLayoutConstraint *cn =
-//        [NSLayoutConstraint constraintWithItem:firstButton
-//                                     attribute:NSLayoutAttributeCenterX
-//                                     relatedBy:NSLayoutRelationEqual
-//                                        toItem:self.view
-//                                     attribute:NSLayoutAttributeCenterX
-//                                    multiplier:1.0
-//                                      constant:0.0];
-//    [self.view addConstraint:cn];
-//    
-//    cn =
-//        [NSLayoutConstraint constraintWithItem:firstButton
-//                                     attribute:NSLayoutAttributeBottom
-//                                     relatedBy:NSLayoutRelationEqual
-//                                        toItem:firstButton.superview
-//                                     attribute:NSLayoutAttributeBottom
-//                                    multiplier:1.0
-//                                      constant:-10.0];
-//    
-//    [self.view addConstraint:cn];
+    UILabel *firstLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 200, 200, 44)];
+    firstLabel.text = @"Hello I am a UILabel";
+    [self.view addSubview:firstLabel];
 }
 
 #define ARC4RANDOM_MAX      0x100000000
 
-- (IBAction)buttonPressed:(UIButton *)sender
+- (void)buttonPressed:(UIButton *)sender
 {
     NSLog(@"Button pressed, sender: %@", sender);
     self.view.alpha = ((double)arc4random() / 0x100000000);
---
+++ Level3Book.md
@@ -0,0 +1,3 @@
+## Level 3 Content
+
+![image](http://developer.apple.com/library/ios/DOCUMENTATION/WindowsViews/Conceptual/ViewControllerCatalog/Art/NavigationViews.png)
