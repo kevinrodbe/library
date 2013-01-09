373e91b226c132e34f84ca379788c0ea22effb05
Casey Jenks <casey@codeshout.com>
2013-01-09 10:42:56 -0500

    slide 3-optional-routes

---
+++ app/assets/javascripts/slides_two/3-optional-routes.js
@@ -0,0 +1,17 @@
+createSlide('3-optional-routes', function(){
+  window.TodoRouter = new (Backbone.Router.extend({
+
+    routes: {
+      'search/:query(/:page)': 'search'
+    },
+
+    search: function(query, page) {
+      page = page || 0;
+      console.log(query);
+      console.log(page);
+    }
+
+  }));
+
+  $(function(){ Backbone.history.start() });
+});
