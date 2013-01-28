12896798aca057f72d9ba2ddd94f0677f342b326
Eric Allam <rubymaverick@gmail.com>
2013-01-28 10:43:21 -0500

    Updated 2-4

--- app/assets/javascripts/challenges/two/2-4.js
+++ app/assets/javascripts/challenges/two/2-4.js
@@ -4,9 +4,9 @@ app.createChallenge('2-4', function(){
    // Instructions:
   
    // We can limit the number of appointments returned 
-   // by passing in a limit parameter also. Go ahead and 
+   // by passing in a per_page parameter also. Go ahead and 
    // construct the fetch call below to create a URL
-   // that looks like `/appointments?since=2013-01-01&limit=10`
+   // that looks like `/appointments?since=2013-01-01&per_page=10`
 
   // Initial code
   var appointments = new Appointments();
@@ -14,5 +14,5 @@ app.createChallenge('2-4', function(){
 
   // Possible Answers:
   var appointments = new Appointments();
-  appointments.fetch({data: {since: "2013-01-01", limit: 10}});
+  appointments.fetch({data: {since: "2013-01-01", per_page: 10}});
 });
