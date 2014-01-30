16e4f970413b7fe7e771676de98dce09df6154f1
Jon Friskics <jonfriskics@gmail.com>
2014-01-29 23:31:51 -0500

    level7 initial commit

---
+++ cs-maps-level7/cs-maps-level7/CSDirectionsCell.h
@@ -0,0 +1,16 @@
+//
+//  CSDirectionsCell.h
+//  cs-maps-level7
+//
+//  Created by Jon Friskics on 1/24/14.
+//  Copyright (c) 2014 Code School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSDirectionsCell : UITableViewCell
+
+@property (strong, nonatomic) UILabel *directionsLabel;
+@property (strong, nonatomic) UILabel *distanceLabel;
+
+@end
